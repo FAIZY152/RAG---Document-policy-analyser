97cd0c4b00cfc1f0530bd362146d526a8b5ff0bc
FAIZY152 <fayazjani7576@gmail.com>
2025-09-12 16:13:42 +0500

    explore-groq-ai

--- src/9-hr/lec-1.js
+++ src/9-hr/lec-1.js
@@ -4,44 +4,44 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const API_KEY = process.env.API_KEY;
-// const ai = new GoogleGenAI({ apiKey: API_KEY });
+const API_KEY = process.env.API_KEY;
+const ai = new GoogleGenAI({ apiKey: API_KEY });
 
-// async function main() {
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: "hello i am a developer and i want to learn about GenAI",
-//   });
-//   console.log(response.text);
-// }
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "hello i am a developer and i want to learn about GenAI",
+  });
+  console.log(response.text);
+}
 
-// await main();
+await main();
 
-// import Groq from "groq-sdk";
+import Groq from "groq-sdk";
 
-// const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-// export async function getGroqChatCompletion() {
-//   const response = await groq.chat.completions.create({
-//     messages: [
-//       {
-//         role: "system",
-//         content: `You are a helpful assistant.your role is islamic scholar and you are here to help with islamic questions and answers and you are here to help with islamic questions and answers
-//             You are a helpful assistant. Your role is to provide accurate and respectful answers to Islamic questions.
-//             You are a Islamic scholar. Your role is to Help the how to get ride from sins and how to get closer to Allah and how to be a good muslim and how to be a good person and how to be a good human being.
-//             You are a helpful assistant. Your Role is to Help the user how to quit Pornography and how to quit Masturbation and how to quit bad habits and how to quit bad things and how to quit bad actions and how to quit bad deeds. 
+export async function getGroqChatCompletion() {
+  const response = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are a helpful assistant.your role is islamic scholar and you are here to help with islamic questions and answers and you are here to help with islamic questions and answers
+            You are a helpful assistant. Your role is to provide accurate and respectful answers to Islamic questions.
+            You are a Islamic scholar. Your role is to Help the how to get ride from sins and how to get closer to Allah and how to be a good muslim and how to be a good person and how to be a good human being.
+            You are a helpful assistant. Your Role is to Help the user how to quit Pornography and how to quit Masturbation and how to quit bad habits and how to quit bad things and how to quit bad actions and how to quit bad deeds. 
 
-//             You are a helpful assistant. Your role is to provide islamic perspectives on various issues and Quit Porn.`,
-//       },
-//       {
-//         role: "user",
-//         content: "how are you and what is your purpose i mean role?",
-//       },
-//     ],
-//     model: "llama-3.3-70b-versatile",
-//   });
+            You are a helpful assistant. Your role is to provide islamic perspectives on various issues and Quit Porn.`,
+      },
+      {
+        role: "user",
+        content: "how are you and what is your purpose i mean role?",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
 
-//   console.log("Groq Chat Completion:", response.choices[0].message.content);
-// }
+  console.log("Groq Chat Completion:", response.choices[0].message.content);
+}
 
 getGroqChatCompletion();
--- src/9-hr/lec-2.js
+++ src/9-hr/lec-2.js
@@ -4,3 +4,20 @@ import Groq from "groq-sdk";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+const GetGroqChatCompletion = async () => {
+  const completion = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    messages: [
+      {
+        role: "user",
+        content: "Hi i am Fayaz",
+      },
+    ],
+  });
+  //   console.log(completion.choices[0].message.content);
+  console.log(completion);
+
+  //   return completion.choices[0].message.content;
+};
+
+GetGroqChatCompletion();
