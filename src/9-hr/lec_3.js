3d47189f77da8fd836641a3b57d2700802ab7620
FAIZY152 <fayazjani7576@gmail.com>
2025-09-16 11:25:43 +0500

    web-calling-through-tavilyy

--- src/9-hr/lec_3.js
+++ src/9-hr/lec_3.js
@@ -2,6 +2,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
@@ -12,16 +13,62 @@ const ToolCall1 = async () => {
     messages: [
       {
         role: "system",
-        content:
-          "you are the smart personal assistant. who answer the asked questions",
+        content: `You are a smart personal assistant. You must use the available tools to answer questions that require real-time or latest information. Always use webSearch for questions about current events, product launches, or recent information.`,
       },
       {
         role: "user",
         content: "when was Iphone 17 launch?",
       },
     ],
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "Your purpose is search the latest information and real time data from the internet.",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search to perfome search on the internet.",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "required",
   });
-  console.log("Ans: ", completion.choices[0].message.content);
+  //   console.log("Ans: ", completion.choices[0].message.tool_calls);
+
+  let tools = completion.choices[0].message.tool_calls;
+  //   console.log("Tools", tools);
+
+  if (!tools) {
+    console.log(`Ans : ${completion.choices[0].message}`);
+    return;
+  }
+
+  for (let tool of tools) {
+    const functionName = tool.function.name;
+    const functionArgs = JSON.parse(tool.function.arguments);
+    if (functionName === "webSearch") {
+      const toolRes = await webSearch(functionArgs);
+      console.log("Result", toolRes);
+    }
+  }
+};
+
+
+const tvly = tavily({ apiKey: "" });
+
+const webSearch = ({ query }) => {
+  console.log("tool function is calling");
+
+  return `Searched result for ${query}`;
 };
 
 ToolCall1();
