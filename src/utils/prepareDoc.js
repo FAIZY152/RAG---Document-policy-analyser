ae7777531d294ff5ab01f725ecee28131e17e34f
FAIZY152 <fayazjani7576@gmail.com>
2025-09-19 16:50:18 +0500

    create-chunks-from-docs

--- src/utils/prepareDoc.js
+++ src/utils/prepareDoc.js
@@ -1,5 +1,7 @@
 import path from "path";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { TokenTextSplitter } from "@langchain/textsplitters";
+import * as fs from "node:fs";
 
 export async function loadPDF(relativePath) {
   // Resolve to absolute path
@@ -11,7 +13,17 @@ export async function loadPDF(relativePath) {
 
   // Load and return docs
   const docs = await loader.load();
-  console.log("📄 Docs loaded:", docs[0].pageContent);
+  const document = docs[0].pageContent;
+  //   console.log("📄 Docs loaded:", docs[0].pageContent);
+
+  const textSplitter = new TokenTextSplitter({
+    chunkSize: 300,
+    chunkOverlap: 80,
+  });
+
+  const texts = await textSplitter.splitText(document);
+  console.log(texts.length, "texts");
+  fs.writeFileSync("src/utils/texts.json", JSON.stringify(texts));
 
   return docs;
 }
